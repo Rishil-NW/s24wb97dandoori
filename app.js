4ef72782ee6bd0a4b053533d20b9955dd9abaf0b
Rishil-NWM <156384714+Rishil-NWM@users.noreply.github.com>
2024-03-26 21:25:50 -0500

    first change

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var RobotsRouter = require('./routes/Robots');
+var gridRouter = require('./routes/grid');
+var pickRouter = require('./routes/pick');
 
 var app = express();
 
@@ -23,7 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/Robots', RobotsRouter);
-
+app.use('/grid', gridRouter);
+app.use('/pick', pickRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
